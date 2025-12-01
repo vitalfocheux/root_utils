d4192c02e7792f0be36e066b2d8e2dd0ba9dcf19
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-12-01 17:37:38 +0100

    ajout des collections

--- lib/root_utils.dart
+++ lib/root_utils.dart
@@ -1,5 +1,7 @@
 library;
 
+export 'dart:collection';
+
 export 'src/AOCD/localstorage.dart';
 export 'src/AOCD/models.dart';
 export 'src/AOCD/plumbing.dart';
@@ -8,3 +10,5 @@ export 'src/extension/string_extension.dart';
 export 'src/math/area.dart';
 export 'src/math/pgcd.dart';
 export 'src/math/vec2.dart';
+export 'src/math/matrix.dart';
+export 'src/graph.dart';
