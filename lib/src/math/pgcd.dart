ed400ad05327c74459e86e7dd7ac53e19a9c43b3
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-12-28 20:43:51 +0100

    modif

--- lib/src/math/pgcd.dart
+++ lib/src/math/pgcd.dart
@@ -1,20 +1,20 @@
-int pgcd(int a, int b) {
+num pgcd(num a, num b) {
   return b == 0 ? a.abs() : pgcd(b, a % b);
 }
 
-int ppcm(int a, int b) {
+num ppcm(num a, num b) {
   return b == 0 ? 0 : (a * b).abs() ~/ pgcd(a, b);
 }
 
-extension LcmExtension on List<int> {
-  int getPGCD() {
+extension LcmExtension on List<num> {
+  num getPGCD() {
     if(isEmpty){
       throw Exception('List is empty');
     }
     return reduce((a, b) => pgcd(a, b));
   }
 
-  int getPPCM() {
+  num getPPCM() {
     if(isEmpty){
       throw Exception('List is empty');
     }
