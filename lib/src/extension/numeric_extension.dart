ff07f282e1a6649ba6d1301d0410967a9bba5a67
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-12-30 21:57:04 +0100

    Ajout de la fonction length & des extensions pour les nombres premiers

--- lib/src/extension/numeric_extension.dart
+++ lib/src/extension/numeric_extension.dart
@@ -1,6 +1,96 @@
 import 'dart:math';
 import 'string_extension.dart';
 
+extension BigPrime on BigInt {
+  bool isPrime() {
+    if (this < BigInt.from(2)) {
+      return false;
+    }
+    for (BigInt i = BigInt.from(2); i * i <= this; i = i + BigInt.one) {
+      if (this % i == BigInt.zero) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  List<BigInt> primeFactor(){
+    List<BigInt> res = [];
+    BigInt i = BigInt.from(2);
+    BigInt n = this;
+    while(n > BigInt.one){
+      while(n % i == BigInt.zero){
+        res.add(i);
+        n = n ~/ i;
+      }
+      i = i + BigInt.one;
+    }
+    return res;
+  }
+
+  static BigInt nthPrime(num n){
+    if(n < 1){
+      throw ArgumentError('n doit être supérieur ou égal à 1');
+    }
+    int count = 0;
+    BigInt num = BigInt.from(2);
+    while(true){
+      if(num.isPrime()){
+        count++;
+        if(count == n){
+          return num;
+        }
+      }
+      num = num + BigInt.one;
+    }
+  }
+}
+
+extension Prime on num {
+  bool isPrime() {
+    if (this < 2) {
+      return false;
+    }
+    for (int i = 2; i <= sqrt(this); i++) {
+      if (this % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  List<int> primeFactor(){
+    List<int> res = [];
+    int i = 2;
+    int n = toInt();
+    while(n > 1){
+      while(n % i == 0){
+        res.add(i);
+        n = n ~/ i;
+      }
+      i++;
+    }
+    return res;
+  }
+
+  static int nthPrime(num n){
+    if(n < 1){
+      throw ArgumentError('n doit être supérieur ou égal à 1');
+    }
+    int count = 0;
+    int num = 2;
+    while(true){
+      if(num.isPrime()){
+        count++;
+        if(count == n){
+          return num;
+        }
+      }
+      num++;
+    }
+  }
+}
+
 extension NumericUtils on num {
 
   bool isPalindrome(){
@@ -9,6 +99,11 @@ extension NumericUtils on num {
 
   int reverse(){
     return toString().reverse().toInt();
+  }
+
+  /// Retourne le nombre de chiffres du nombre
+  int get length {
+    return toString().length;
   }
 
   /// Vérifie si le nombre est constitué de la concaténation de deux fois le même nombre
@@ -28,17 +123,6 @@ extension NumericUtils on num {
     return n % half == 0;
   } 
 
-  bool isPrime(){
-    if(this < 2){
-      return false;
-    }
-    for(int i = 2; i <= sqrt(this); i++){
-      if(this % i == 0){
-        return false;
-      }
-    }
-    return true;
-  }
 
   bool isBetween(num min, num max, {bool inclusive = true}){
     if(inclusive){
@@ -46,20 +130,6 @@ extension NumericUtils on num {
     } else {
       return this > min && this < max;
     }
-  }
-
-  List<int> primeFactor(){
-    List<int> res = [];
-    int i = 2;
-    int n = toInt();
-    while(n > 1){
-      while(n % i == 0){
-        res.add(i);
-        n = n ~/ i;
-      }
-      i++;
-    }
-    return res;
   }
 
   List<int> getDivisors(){
